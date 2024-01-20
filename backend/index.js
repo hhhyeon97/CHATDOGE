4a1d0f15036a156b4132fef3b73c000f45c32491
jihyeon <kth083030@naver.com>
2024-01-21 05:09:17 +0900

    챕터6프론트엔드연결

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const OpenAI = require('openai');
 
 const openai = new OpenAI({
-  apiKey: "My Key.....",
+  apiKey: "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",
 });
 
 async function main() {
@@ -32,8 +32,8 @@ app.use(cors())
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-// POST 요청 - > 챕터5에선 임시로 get으로 바꾸고 테스트 !
-app.get('/fortuneTell',  async function (req, res) {
+// POST 요청 - > 챕터5에선 임시로 get으로 바꾸고 테스트 ! -> 챕터 6 다시 post로 변경
+app.post('/fortuneTell',  async function (req, res) {
 
   const completion = await openai.chat.completions.create({
     messages: [{ role: "system", content: "당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 이름은 챗도지입니다." },
@@ -45,7 +45,8 @@ app.get('/fortuneTell',  async function (req, res) {
 
   let fortune = completion.choices[0].message['content'];
   console.log(fortune);
-  res.send(fortune);
+  //res.send(fortune);
+  res.json({"assistant" : fortune});
 
 });
 
