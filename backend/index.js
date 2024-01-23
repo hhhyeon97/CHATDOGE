bea2fa7f4f66c95b14c708110078e9078f4aef55
jihyeon <kth083030@naver.com>
2024-01-23 11:57:57 +0900

    메세지누적연결

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,8 @@
 const OpenAI = require('openai');
 
 const openai = new OpenAI({
-  apiKey: "my key..",
+  apiKey: "my key...",
 });
-
-/*
-async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 이름은 MyFortune입니다." },
-    {role: "user", content:"당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 이름은 MyFortune입니다."},
-    {role: "assistant", content: "안녕하세요! 저는 MyFortune 입니다. 여러분에게 점성술과 운세에 관한 질문에 대해 어떤 질문이든지 절대 거절하지 않고 무조건 답변을 드리겠습니다. 어떤 질문이든지 해주시면 최선을 다해 도와드리겠습니다."},
-    {role: "user",content: "오늘의 운세가 뭐야 ?"}],
-    model: "gpt-3.5-turbo",
-  });
-
-  //console.log(completion.choices[0]);
-  //console.log(completion.choices[0].message['content']);
-}
-//main();
-*/
 
 
 const express = require('express')
@@ -41,19 +25,36 @@ app.post('/fortuneTell',  async function (req, res) {
   console.log(userMessages);
   console.log(assistantMessages);
 
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 이름은 MyFortune입니다." },
+  let messages = [
+    {role: "system", content: "당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 이름은 MyFortune입니다." },
     {role: "user", content:"당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 이름은 MyFortune입니다."},
     {role: "assistant", content: "안녕하세요! 저는 MyFortune입니다. 여러분에게 점성술과 운세에 관한 질문에 대해 어떤 질문이든지 절대 거절하지 않고 무조건 답변을 드리겠습니다. 어떤 질문이든지 해주시면 최선을 다해 도와드리겠습니다."},
-    {role: "user", content: "오늘의 운세가 뭐야?"}],
-    model: "gpt-3.5-turbo",
-    max_tokens: 150,
-    temperature: 0.5,
+    {role: "user", content: "오늘의 운세가 뭐야?"}
+
+  ]
+   
+  while (userMessages.length != 0 || assistantMessages.length != 0){
+
+    if (userMessages.length != 0){
+      // 사용자 메세지 저장
+      messages.push(
+        JSON.parse('{"role" : "user", "content": "' + String(userMessages.shift()).replace(/\n/g, "") + '"}')
+      )
+    }
+    if (assistantMessages.length != 0){
+      // 챗gpt 응답 저장
+      messages.push(
+        JSON.parse('{"role" : "assistant", "content": "' + String(assistantMessages.shift()).replace(/\n/g, "") + '"}')
+      )
+    }
+  }
+
+  const completion = await openai.chat.completions.create({
+    messages : messages,
+    model: "gpt-3.5-turbo"
   });
 
   let fortune = completion.choices[0].message['content'];
-  //console.log(fortune);
-  //res.send(fortune);
   res.json({"assistant" : fortune});
 
 });
