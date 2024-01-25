67dd50a9a4841719f832297059931b9fec6a330d
jihyeon <kth083030@naver.com>
2024-01-26 08:37:17 +0900

    2차수정완료/챗ui출력해결필요

--- frontend/script.js
+++ frontend/script.js
@@ -1,101 +1,89 @@
-
-//변수 생성
+// 변수 생성
 let userMessages = [];
 let assistantMessages = [];
 
 let myDateTime = '';
 
-function start(){
-    const date = document.getElementById('date').value;
-    const hour = document.getElementById('hour').value;
-    if(date === ''){
-        alert('생년월일을 입력해 주세요 !');
-        return;
-    }
-    myDateTime = date + hour;
-    
-    document.getElementById("intro").style.display = "none";
-    document.getElementById("chat").style.display = "block";
-    document.getElementById("ad").style.display = "none";
+function start() {
+  const date = document.getElementById('date').value;
+  const hour = document.getElementById('hour').value;
+  if (date === '') {
+    alert('생년월일을 입력해 주세요 !');
+    return;
+  }
+  myDateTime = date + hour;
 
-    //console.log(myDateTime);
+  document.getElementById("intro").style.display = "none";
+  document.getElementById("chat").style.display = "block";
+  document.getElementById("ad").style.display = "none";
+  document.getElementById("in").style.display = "block";
 }
 
-
 function handleKeyPress(event) {
-    // 엔터 키를 눌렀을 때
-    if (event.key === 'Enter') {
-      sendMessage();
-    }
+  // 엔터 키를 눌렀을 때
+  if (event.key === 'Enter') {
+    sendMessage();
   }
+}
 
 async function sendMessage() {
-    const m = document.getElementById('messageInput').value.trim();
-    if (m === '') {
-        alert('메세지를 입력해주세요!');
-        document.getElementById('messageInput').focus();
-        return;
+  const messageInput = document.getElementById('messageInput');
+  const message = messageInput.value.trim();
+  if (message === '') {
+    alert('메세지를 입력해주세요!');
+    messageInput.focus();
+    return;
+  }
+
+  // 로딩 스피너 보여주기 - > 로딩 메세지 보여주기 
+  document.getElementById('loader-message').style.display = "block";
+
+  // 사용자 메세지 출력
+  const userBubble = document.createElement('div');
+  userBubble.className = 'chat-bubble user-bubble';
+  userBubble.textContent = message;
+  document.getElementById('fortuneResponse').appendChild(userBubble);
+
+  // 사용자 메세지 저장
+  userMessages.push(message);
+
+  // 입력 필드 초기화
+  messageInput.value = '';
+
+  // 백엔드 서버에 메세지를 보내고 응답 출력
+  try {
+    const response = await fetch('https://totqmestykrevynhnnuolqpsju0ebsbo.lambda-url.ap-northeast-2.on.aws/fortuneTell', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        myDateTime: myDateTime,
+        userMessages: userMessages,
+        assistantMessages: assistantMessages,
+      })
+    });
+
+    if (!response.ok) {
+      throw new Error('Request failed with status ' + response.status);
     }
-    
-    // 로딩 스피너 보여주기
-    document.getElementById('loader').style.display = "block";
 
-    // 사용자 메세지 가져옴
-    const messageInput = document.getElementById('messageInput');
-    const message = messageInput.value;
+    const data = await response.json();
+    // console.log('Response:', data.assistant);
 
-    //채팅 말풍선에 사용자의 메세지 출력
-    const userBubble = document.createElement('div');
-    userBubble.className = 'chat-bubble user-bubble';
-    userBubble.textContent = message;
-    document.getElementById('fortuneResponse').appendChild(userBubble);
+    // 로딩 스피너 숨기기 - > 로딩 메세지 숨기기 
+    document.getElementById('loader-message').style.display = "none";
 
-    //userMessages에 사용자의 메세지 저장
-    userMessages.push(messageInput.value);
+    // 채팅 말풍선에 챗gpt 응답 출력
+    const botBubble = document.createElement('div');
+    botBubble.className = 'chat-bubble bot-bubble';
+    botBubble.textContent = data.assistant;
+    document.getElementById('fortuneResponse').appendChild(botBubble);
 
+    // assistantMessages에 챗gpt 메세지 저장
+    assistantMessages.push(data.assistant);
 
-    //입력 필드 초기화
-    messageInput.value = '';
-
-    //백엔드 서버에 메세지를 보내고 응답 출력
-    try {
-
-        const response = await fetch('https://totqmestykrevynhnnuolqpsju0ebsbo.lambda-url.ap-northeast-2.on.aws/fortuneTell', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                //'Origin': 'https://myfortune.pages.dev'  // 슬래시 제거
-            },
-            body: JSON.stringify({
-                myDateTime: myDateTime,
-                userMessages: userMessages,
-                assistantMessages: assistantMessages,
-            })
-        });
-        if (!response.ok){
-            throw new Error('Request failed with status '+response.status);
-        }
-        const data = await response.json();        
-        //console.log('Response:', data.assistant);
-
-        //로딩 스피너 숨기기
-        document.getElementById('loader').style.display = "none";
-
-        assistantMessages.push(data.assistant);
-        console.log('Response:', data);
-
-        
-        //채팅 말풍선에 챗gpt 응답 출력
-         const botBubble = document.createElement('div');
-         botBubble.className = 'chat-bubble bot-bubble';
-         botBubble.textContent = data.assistant;
-         document.getElementById('fortuneResponse').appendChild(botBubble);
-
-        //assistantMessages에 챗gpt 메세지 저장
-        assistantMessages.push(data.assistant);
-
-
-    }catch(error){
-        console.error('Error : ',error);
-    }
+  } catch (error) {
+    console.error('Error : ', error);
+  }
 }
